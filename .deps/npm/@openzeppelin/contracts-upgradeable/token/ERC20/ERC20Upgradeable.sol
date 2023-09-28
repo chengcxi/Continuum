aa6823c27996844eba6ebe8101fd32edb159c341
chengcxi <143553765+chengcxi@users.noreply.github.com>
2023-09-28 12:03:12 -0700

    Update OpenZeppelin lib

--- .deps/npm/@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol
+++ .deps/npm/@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 // OpenZeppelin Contracts (last updated v4.9.0) (token/ERC20/ERC20.sol)
 
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.21;
 
 import "./IERC20Upgradeable.sol";
 import "./extensions/IERC20MetadataUpgradeable.sol";
